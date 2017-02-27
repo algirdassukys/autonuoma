22ea06f1a4f356101531014063478a8a05aceaa8
algirdassukys <sukys.algirdas@gmail.com>
2017-02-27 18:33:43 +0200

    Add index file

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+	// nuskaitome konfigūracijų failą
+	include 'config.php';
+
+	// iškviečiame prisijungimo prie duomenų bazės klasę
+	include 'utils/mysql.class.php';
+	
+	// nustatome pasirinktą modulį
+	$module = '';
+	if(isset($_GET['module'])) {
+		$module = mysql::escape($_GET['module']);
+	}
+	
+	// jeigu pasirinktas elementas (sutartis, automobilis ir kt.), nustatome elemento id
+	$id = '';
+	if(isset($_GET['id'])) {
+		$id = mysql::escape($_GET['id']);
+	}
+	
+	// nustatome, ar kuriamas naujas elementas
+	$action = '';
+	if(isset($_GET['action'])) {
+		$action = mysql::escape($_GET['action']);
+	}
+	
+	// jeigu šalinamas elementas, nustatome šalinamo elemento id
+	$removeId = 0;
+	if(!empty($_GET['remove'])) {
+		// paruošiame $_GET masyvo id reikšmę SQL užklausai
+		$removeId = mysql::escape($_GET['remove']);
+	}
+		
+	// nustatome elementų sąrašo puslapio numerį
+	$pageId = 1;
+	if(!empty($_GET['page'])) {
+		$pageId = mysql::escape($_GET['page']);
+	}
+	
+	// nustatome, kiek įrašų rodysime elementų sąraše
+	define('NUMBER_OF_ROWS_IN_PAGE', 2);
+	
+	// nustatome, kurį veiksmų failą įtraukti į šabloną main.tpl.php
+	$actionFile = "";
+	if(!empty($module) && !empty($action)) {
+		$actionFile = "actions/{$module}_{$action}.php";
+	}
+	
+	// įtraukiame pagrindinį šabloną
+	include 'templates/main.tpl.php';
+?>
