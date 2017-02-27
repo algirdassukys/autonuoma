fe9c1992b851790ae9325e572ebf86ab4b7a8859
algirdassukys <sukys.algirdas@gmail.com>
2017-02-27 20:01:24 +0200

    Pakeistas prefiksas antrą kartą.

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ class config {
 	const DB_NAME      = 'autonuoma_min';
 	const DB_USERNAME  = 'root';
 	const DB_PASSWORD  = '';
-	const DB_PREFIX	   = 'pavyzdys_';
+	const DB_PREFIX	   = 'pavyzdys_3';
 	
 	const NUMBER_OF_ROWS_IN_PAGE = 10;
 }
