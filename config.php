9ad42aa91f2a83b8687faa6ddfa6796f7b32a4fa
Algirdas Šukys <algirdas.sukys@ktu.lt>
2023-02-26 17:57:11 +0200

    Set default database password to root.

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ class config {
 	const DB_SERVER    = '127.0.0.1';
 	const DB_NAME      = 'autonuoma';
 	const DB_USERNAME  = 'root';
-	const DB_PASSWORD  = '';
+	const DB_PASSWORD  = 'root';
 	const DB_PREFIX	   = 'pavyzdys_';
 	const NUMBER_OF_ROWS_IN_PAGE = 10;
 }
